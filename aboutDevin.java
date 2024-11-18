276e5755966439ce4e6d55e0afafb4c73f38ce63
Devin R <dvnrt.25@gmail.com>
2024-11-18 12:20:40 +0700

    Introduction: Devin

---
+++ aboutDevin.java
@@ -0,0 +1,6 @@
+public class aboutDevin {
+    public static void main(String[] args) {
+        System.out.println("Hello, the name is Devin Rianto");
+        System.out.println("My NIM is 244107020138");
+    }
+}
